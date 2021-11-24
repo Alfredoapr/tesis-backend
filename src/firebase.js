42ed1f6b7b8526f45185d5da79f204b6e6f57d93
Luis Resendiz <luisresendiz734@gmail.com>
2021-11-24 15:36:48 -0600

    Firebase credentials

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBot4qnvrHrjGV_JvbUozIzdFAO6VtvjPE",
+  authDomain: "tesis-4a222.firebaseapp.com",
+  projectId: "tesis-4a222",
+  storageBucket: "tesis-4a222.appspot.com",
+  messagingSenderId: "343636294944",
+  appId: "1:343636294944:web:11e683e126ea3b98222b45",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
